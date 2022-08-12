093efa7cffc43c873c3c70b6d83959e9d728be1a
thekatiedee <katie.m.diamond@gmail.com>
2022-08-12 14:58:24 -0400

    updating condit_highlight

--- condit_highlight.py
+++ condit_highlight.py
@@ -25,15 +25,17 @@ def condit_highlight():
     # openpyxl way of opening a workbook
     wb = load_workbook(excel)
     ws = wb.active
-    # highlight cell yellow
+    # highlight fave books yellow
     yellow_fill = PatternFill(
         start_color="FFFF00", end_color="FFFF00", fill_type="solid"
     )
+    # highlight fave authors pink
+    pink_fill = PatternFill(start_color="FF69B4", end_color="FF69B4", fill_type="solid")
+
     new_style = DifferentialStyle(fill=yellow_fill)
     rule = Rule(type="expression", dxf=new_style, stopIfTrue=True)
     sheet1 = wb["books"]
     sheet2 = wb["favorites"]
-    print(sheet2.values)
 
     # sheet2 fave books list
     fave_books = []
@@ -43,7 +45,7 @@ def condit_highlight():
     # sheet2 authors list
     fave_authors = []
     for column in sheet2["B"]:
-        fave_books.append(column.value)
+        fave_authors.append(column.value)
 
     # highlighting all the faves_books:
     for rows in sheet1.iter_rows(min_row=1, max_row=15, min_col=0, max_col=3):
@@ -58,8 +60,23 @@ def condit_highlight():
         for cell in rows:
             # if statement works now!
             if cell.value in fave_authors:
-                cell.fill = yellow_fill
+                cell.fill = pink_fill
                 wb.save("my_test.xlsx")
+
+    # create a new sheet with the key
+    wb.create_sheet("key")
+    sheet3 = wb["key"]
+    # yellow are the books, pink are the authors
+    # intro
+    intro = sheet3["A1"]
+    intro.value = "this is a test"
+
+    # yellow books
+
+    cell.fill = yellow_fill
+
+    # pink authors
+    wb.save("my_test.xlsx")
 
 
 if __name__ == "__main__":
